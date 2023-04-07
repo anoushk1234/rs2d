eebc84b817e39770fa18928bf01a47f6ee0dfb64
anoushk1234 <anoushka1828@gmail.com>
2023-04-07 12:56:28 +0530

    clone

--- src/lib.rs
+++ src/lib.rs
@@ -26,11 +26,11 @@ pub fn encode(data: Vec<Vec<u8>>, shard_len: usize) {
 
     let mut sq_root_rounded = sq_root.ceil() as usize; // 45
                                                        // print all thre variables above
-    println!(
-        "data_array_length: {:?} sq_root: {:?} sq_root_rounded: {:?}",
-        data_array_length, sq_root, sq_root_rounded
-    );
-    if sq_root_rounded % 5 == 0 {
+                                                       // println!(
+                                                       //     "data_array_length: {:?} sq_root: {:?} sq_root_rounded: {:?}",
+                                                       //     data_array_length, sq_root, sq_root_rounded
+                                                       // );
+    if sq_root_rounded % 2 != 0 {
         sq_root_rounded += 1;
     }
     let mut data_matrix = vec![vec![vec![0; shard_len]; sq_root_rounded]; sq_root_rounded];
@@ -45,19 +45,26 @@ pub fn encode(data: Vec<Vec<u8>>, shard_len: usize) {
         data_matrix[i].append(&mut parity_prefill);
     }
 
-    let mut m = Matrix::default();
-    m.data = data_matrix.clone();
-    m.row_count = sq_root_rounded;
-    m.col_count = sq_root_rounded;
-
-    for mut row in data_matrix.clone() {
+    // let mut m = Matrix::default();
+    // m.data = data_matrix.clone();
+    // m.row_count = sq_root_rounded;
+    // m.col_count = sq_root_rounded;
+    // println!("data_matrix: {:?}", data_matrix[data_matrix.len() - 1]);
+    let mut new_data_matrix: Vec<Vec<&mut [u8]>> = vec![];
+    for row in data_matrix.clone() {
         let r = ReedSolomon::new(sq_root_rounded, sq_root_rounded).unwrap(); // assuming n:k is 1:1
-        m.erasure_matrix.push(r.clone());
+        let mut row = row.clone(); // m.erasure_matrix.push(r.clone());
 
         let mut x: Vec<&mut [u8]> = row.iter_mut().map(|f| f.as_mut_slice()).collect();
+
         r.encode(&mut x).unwrap();
+
+        new_data_matrix.push(x);
     }
     println!("data_matrix: {:?}", data_matrix);
+    // for i in 0..sq_root_rounded {
+    //   let next_layer = vec![vec![0u8; shard_len]; sq_root_rounded];
+    // }
 }
 
 pub fn decode() {}
@@ -70,7 +77,7 @@ mod tests {
     fn test_encode() {
         // make a 2d vec of 23 numbers from 1 to 23
         let mut original = vec![];
-        let data_len = 10;
+        let data_len = 9;
         let shard_len = 10;
         for _ in 0..data_len {
             let mut elem: Vec<u8> = Vec::with_capacity(shard_len as usize);
